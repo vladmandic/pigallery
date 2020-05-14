896a0542530c224e9e0465fc2bf98eba9c81eaf0
Vladimir Mandic <cyan00@gmail.com>
2020-05-13 21:29:58 -0400

    implemented real-time video processing

--- client/log.js
+++ client/log.js
@@ -3,14 +3,14 @@ import * as tf from '@tensorflow/tfjs';
 const div = {};
 
 async function result(...msg) {
-  if (div) div.Log.innerHTML += `${msg}<br>`;
+  if (div && div.Log) div.Log.innerHTML += `${msg}<br>`;
   // eslint-disable-next-line no-console
   console.log(...msg);
 }
 
 async function active(...msg) {
   const mem = await tf.memory();
-  if (div) div.Active.innerHTML = `${msg}<br>Memory State: Bytes:${mem.numBytes.toLocaleString()} Buffers:${mem.numDataBuffers.toLocaleString()} Tensors:${mem.numTensors.toLocaleString()}`;
+  if (div && div.Active) div.Active.innerHTML = `${msg}<br>Memory State: Bytes:${mem.numBytes.toLocaleString()} Buffers:${mem.numDataBuffers.toLocaleString()} Tensors:${mem.numTensors.toLocaleString()}`;
   // eslint-disable-next-line no-console
   else console.log(...msg);
 }
