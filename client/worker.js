541e5759b1aec58b73ff631be869d8f5404e5df8
Vladimir Mandic <cyan00@gmail.com>
2020-06-21 09:50:01 -0400

    moved long processing to web worker

--- client/worker.js
+++ client/worker.js
@@ -1,7 +1,27 @@
-const db = require('./indexdb.js');
-const log = require('./log.js');
+const hash = require('./blockhash.js');
 
-onmessage = (msg) => {
-  console.log('Worker', msg);
-  postMessage('worker done');
+onmessage = async (msg) => {
+  // console.log('Worker received message', msg.data);
+  const all = msg.data;
+  let duplicates = [];
+  let duplicate;
+  const length = all.length - 1;
+  for (let i = 0; i < length + 1; i++) {
+    const a = all[i];
+    duplicate = false;
+    for (let j = i + 1; j < length; j++) {
+      const b = all[j];
+      const distance = (a.hash === b.hash) ? 0 : (hash.distance(a.phash, b.phash) + 1);
+      if (distance < 35) {
+        a.simmilarity = distance;
+        b.simmilarity = distance;
+        duplicate = true;
+        duplicates.push(b);
+      }
+    }
+    if (duplicate) duplicates.push(a);
+  }
+  duplicates = [...new Set(duplicates)];
+  duplicates = duplicates.sort((a, b) => (a.simmilarity - b.simmilarity));
+  postMessage(duplicates);
 };
