fdd555ad74fd147e0c3404c5e97284239f8e4351
Vladimir Mandic <mandic00@live.com>
2021-02-23 22:14:35 -0500

    video fix

--- client/video/video.js
+++ client/video/video.js
@@ -1,16 +1,16 @@
 // @ts-nocheck
 
 import $ from 'jquery';
-import Human from '@vladmandic/human/dist/human.esm-nobundle.js';
 import { tf } from '../shared/tf.js';
+// eslint-disable-next-line import/order
+import Human from '@vladmandic/human/dist/human.esm-nobundle.js';
 import panzoom from '../../assets/panzoom.js';
 import * as log from '../shared/log.js';
 import * as user from '../shared/user.js';
 import * as run from './run.js';
 import Menu from '../shared/menu.js';
-import * as shared from '../shared/config.js';
+import * as config from '../shared/config.js';
 
-const config = shared.default;
 let perfMonitor;
 
 // using window globals for debugging purposes
@@ -88,52 +88,52 @@ async function cameraSetup() {
     log.div('div', true, `Start video: ${track.label} camera ${settings.width} x ${settings.height} display ${video.offsetWidth} x ${video.offsetHeight} facing ${settings.facingMode}`);
     log.debug('Camera Settings: ', settings);
     event.stopPropagation();
-    run.main(config, objects);
+    run.main(config.default, objects);
     document.getElementById('status').innerText = 'loading model: Human ...';
   }, true);
 }
 
 function initHumanConfig() {
-  if (!config.human) {
+  if (!config.default.human) {
     const human = new Human();
-    config.human = JSON.parse(JSON.stringify(human.config));
-    config.human.enabled = true;
-    config.human.face.detector.modelPath = '@vladmandic/human/models/blazeface-back.json';
-    config.human.face.mesh.modelPath = '@vladmandic/human/models/facemesh.json';
-    config.human.face.iris.modelPath = '@vladmandic/human/models/iris.json';
-    config.human.face.age.modelPath = '@vladmandic/human/models/age-ssrnet-imdb.json';
-    config.human.face.gender.modelPath = '@vladmandic/human/models/gender-ssrnet-imdb.json';
-    config.human.face.emotion.modelPath = '@vladmandic/human/models/emotion-large.json';
-    config.human.face.embedding.modelPath = '@vladmandic/human/models/mobilefacenet.json';
-    config.human.body.modelPath = '@vladmandic/human/models/posenet.json';
-    config.human.hand.detector.modelPath = '@vladmandic/human/models/handdetect.json';
-    config.human.hand.skeleton.modelPath = '@vladmandic/human/models/handskeleton.json';
+    config.default.human = JSON.parse(JSON.stringify(human.config));
+    config.default.human.enabled = true;
+    config.default.human.face.detector.modelPath = '@vladmandic/human/models/blazeface-back.json';
+    config.default.human.face.mesh.modelPath = '@vladmandic/human/models/facemesh.json';
+    config.default.human.face.iris.modelPath = '@vladmandic/human/models/iris.json';
+    config.default.human.face.age.modelPath = '@vladmandic/human/models/age-ssrnet-imdb.json';
+    config.default.human.face.gender.modelPath = '@vladmandic/human/models/gender.json';
+    config.default.human.face.emotion.modelPath = '@vladmandic/human/models/emotion-large.json';
+    config.default.human.face.embedding.modelPath = '@vladmandic/human/models/mobilefacenet.json';
+    config.default.human.body.modelPath = '@vladmandic/human/models/posenet.json';
+    config.default.human.hand.detector.modelPath = '@vladmandic/human/models/handdetect.json';
+    config.default.human.hand.skeleton.modelPath = '@vladmandic/human/models/handskeleton.json';
   }
 }
 
 async function menuSetup() {
-  if (!config.models) {
+  if (!config.default.models) {
     const req = await fetch('/api/models/get');
-    if (req && req.ok) config.models = await req.json();
+    if (req && req.ok) config.default.models = await req.json();
   }
   initHumanConfig();
   objects.menus.model = new Menu(document.body, '', null, { background: 'var(--body)' });
   objects.menus.model.addLabel('Human Detection');
-  objects.menus.model.addBool('Human Detection', config.human, 'enabled');
-  objects.menus.model.addBool('Face Detect', config.human.face, 'enabled');
-  objects.menus.model.addBool('Face Mesh', config.human.face.mesh, 'enabled');
-  objects.menus.model.addBool('Face Iris', config.human.face.iris, 'enabled');
-  objects.menus.model.addBool('Face Age', config.human.face.age, 'enabled');
-  objects.menus.model.addBool('Face Gender', config.human.face.gender, 'enabled');
-  objects.menus.model.addBool('Face Emotion', config.human.face.emotion, 'enabled');
-  objects.menus.model.addBool('Body Pose', config.human.body, 'enabled');
-  objects.menus.model.addBool('Hand Pose', config.human.hand, 'enabled');
+  objects.menus.model.addBool('Human Detection', config.default.human, 'enabled');
+  objects.menus.model.addBool('Face Detect', config.default.human.face, 'enabled');
+  objects.menus.model.addBool('Face Mesh', config.default.human.face.mesh, 'enabled');
+  objects.menus.model.addBool('Face Iris', config.default.human.face.iris, 'enabled');
+  objects.menus.model.addBool('Face Age', config.default.human.face.age, 'enabled');
+  objects.menus.model.addBool('Face Gender', config.default.human.face.gender, 'enabled');
+  objects.menus.model.addBool('Face Emotion', config.default.human.face.emotion, 'enabled');
+  objects.menus.model.addBool('Body Pose', config.default.human.body, 'enabled');
+  objects.menus.model.addBool('Hand Pose', config.default.human.hand, 'enabled');
 
   objects.menus.model.addLabel('Object Detection');
-  for (const m of config.models.detect) objects.menus.model.addBool(m.name, config.detect, m.name);
+  for (const m of config.default.models.detect) objects.menus.model.addBool(m.name, config.default.detect, m.name);
   objects.menus.model.addLabel('Image Classification');
-  for (const m of config.models.classify) objects.menus.model.addBool(m.name, config.classify, m.name);
-  for (const m of config.models.various) objects.menus.model.addBool(m.name, config.classify, m.name);
+  for (const m of config.default.models.classify) objects.menus.model.addBool(m.name, config.default.classify, m.name);
+  for (const m of config.default.models.various) objects.menus.model.addBool(m.name, config.default.classify, m.name);
 
   objects.menus.model.toggle();
 
@@ -143,60 +143,60 @@ async function menuSetup() {
     log.debug('Changing WebGL: WEBGL_DELETE_TEXTURE_THRESHOLD:', val);
     tf.ENV.set('WEBGL_DELETE_TEXTURE_THRESHOLD', val ? 0 : -1);
   });
-  objects.menus.params.addRange('Max Objects', config.human.face.detector, 'maxFaces', 0, 50, 1, (val) => {
-    config.human.face.detector.maxFaces = parseInt(val);
-    config.human.body.maxDetections = parseInt(val);
-    config.human.hand.maxHands = parseInt(val);
-    config.detect.maxObjects = parseInt(val);
+  objects.menus.params.addRange('Max Objects', config.default.human.face.detector, 'maxFaces', 0, 50, 1, (val) => {
+    config.default.human.face.detector.maxFaces = parseInt(val);
+    config.default.human.body.maxDetections = parseInt(val);
+    config.default.human.hand.maxHands = parseInt(val);
+    config.default.detect.maxObjects = parseInt(val);
   });
-  objects.menus.params.addRange('Skip Frames', config.human.face.detector, 'skipFrames', 0, 50, 1, (val) => {
-    config.human.face.detector.skipFrames = parseInt(val);
-    config.human.face.emotion.skipFrames = parseInt(val);
-    config.human.face.age.skipFrames = parseInt(val);
-    config.human.hand.skipFrames = parseInt(val);
+  objects.menus.params.addRange('Skip Frames', config.default.human.face.detector, 'skipFrames', 0, 50, 1, (val) => {
+    config.default.human.face.detector.skipFrames = parseInt(val);
+    config.default.human.face.emotion.skipFrames = parseInt(val);
+    config.default.human.face.age.skipFrames = parseInt(val);
+    config.default.human.hand.skipFrames = parseInt(val);
   });
-  objects.menus.params.addRange('Min Confidence', config.human.face.detector, 'minConfidence', 0.0, 1.0, 0.05, (val) => {
-    config.human.face.detector.minConfidence = parseFloat(val);
-    config.human.face.emotion.minConfidence = parseFloat(val);
-    config.human.hand.minConfidence = parseFloat(val);
+  objects.menus.params.addRange('Min Confidence', config.default.human.face.detector, 'minConfidence', 0.0, 1.0, 0.05, (val) => {
+    config.default.human.face.detector.minConfidence = parseFloat(val);
+    config.default.human.face.emotion.minConfidence = parseFloat(val);
+    config.default.human.hand.minConfidence = parseFloat(val);
   });
-  objects.menus.params.addRange('Score Threshold', config.human.face.detector, 'scoreThreshold', 0.1, 1.0, 0.05, (val) => {
-    config.human.face.detector.scoreThreshold = parseFloat(val);
-    config.human.hand.scoreThreshold = parseFloat(val);
-    config.human.body.scoreThreshold = parseFloat(val);
-    config.detect.minThreshold = parseFloat(val);
-    config.classify.minThreshold = parseFloat(val);
+  objects.menus.params.addRange('Score Threshold', config.default.human.face.detector, 'scoreThreshold', 0.1, 1.0, 0.05, (val) => {
+    config.default.human.face.detector.scoreThreshold = parseFloat(val);
+    config.default.human.hand.scoreThreshold = parseFloat(val);
+    config.default.human.body.scoreThreshold = parseFloat(val);
+    config.default.detect.minThreshold = parseFloat(val);
+    config.default.classify.minThreshold = parseFloat(val);
   });
-  objects.menus.params.addRange('IOU Threshold', config.human.face.detector, 'iouThreshold', 0.1, 1.0, 0.05, (val) => {
-    config.human.face.detector.iouThreshold = parseFloat(val);
-    config.human.hand.iouThreshold = parseFloat(val);
+  objects.menus.params.addRange('IOU Threshold', config.default.human.face.detector, 'iouThreshold', 0.1, 1.0, 0.05, (val) => {
+    config.default.human.face.detector.iouThreshold = parseFloat(val);
+    config.default.human.hand.iouThreshold = parseFloat(val);
   });
   objects.menus.params.addHTML('<hr style="min-width: 200px; border-style: inset; border-color: dimgray">');
   objects.menus.params.addLabel('Display options');
-  objects.menus.params.addRange('Scale Resolution', config.ui, 'scale', 10, 100, 5);
-  objects.menus.params.addBool('Show Text', config.ui, 'text');
-  objects.menus.params.addBool('Use 3D Depth', config.ui, 'useDepth');
-  objects.menus.params.addBool('Hide Overlay', config.ui, 'overlay');
-  objects.menus.params.addBool('Draw Boxes', config.ui, 'drawBoxes');
-  objects.menus.params.addBool('Draw Points', config.ui, 'drawPoints');
-  objects.menus.params.addBool('Draw Polygons', config.ui, 'drawPolygons');
-  objects.menus.params.addBool('Fill Polygons', config.ui, 'fillPolygons');
+  objects.menus.params.addRange('Scale Resolution', config.default.ui, 'scale', 10, 100, 5);
+  objects.menus.params.addBool('Show Text', config.default.ui, 'text');
+  objects.menus.params.addBool('Use 3D Depth', config.default.ui, 'useDepth');
+  objects.menus.params.addBool('Hide Overlay', config.default.ui, 'overlay');
+  objects.menus.params.addBool('Draw Boxes', config.default.ui, 'drawBoxes');
+  objects.menus.params.addBool('Draw Points', config.default.ui, 'drawPoints');
+  objects.menus.params.addBool('Draw Polygons', config.default.ui, 'drawPolygons');
+  objects.menus.params.addBool('Fill Polygons', config.default.ui, 'fillPolygons');
   objects.menus.params.toggle();
 
   objects.menus.filters = new Menu(document.body, '');
-  objects.menus.filters.addRange('Brightness', config.human.filter, 'brightness', -1.0, 1.0, 0.05, (val) => config.human.filter.brightness = parseFloat(val));
-  objects.menus.filters.addRange('Contrast', config.human.filter, 'contrast', -1.0, 1.0, 0.05, (val) => config.human.filter.contrast = parseFloat(val));
-  objects.menus.filters.addRange('Sharpness', config.human.filter, 'sharpness', 0, 1.0, 0.05, (val) => config.human.filter.sharpness = parseFloat(val));
-  objects.menus.filters.addRange('Blur', config.human.filter, 'blur', 0, 20, 1, (val) => config.human.filter.blur = parseInt(val));
-  objects.menus.filters.addRange('Saturation', config.human.filter, 'saturation', -1.0, 1.0, 0.05, (val) => config.human.filter.saturation = parseFloat(val));
-  objects.menus.filters.addRange('Hue', config.human.filter, 'hue', 0, 360, 5, (val) => config.human.filter.hue = parseInt(val));
-  objects.menus.filters.addRange('Pixelate', config.human.filter, 'pixelate', 0, 32, 1, (val) => config.human.filter.pixelate = parseInt(val));
-  objects.menus.filters.addBool('negative', config.human.filter, 'negative');
-  objects.menus.filters.addBool('sepia', config.human.filter, 'sepia');
-  objects.menus.filters.addBool('vintage', config.human.filter, 'vintage');
-  objects.menus.filters.addBool('kodachrome', config.human.filter, 'kodachrome');
-  objects.menus.filters.addBool('technicolor', config.human.filter, 'technicolor');
-  objects.menus.filters.addBool('polaroid', config.human.filter, 'polaroid');
+  objects.menus.filters.addRange('Brightness', config.default.human.filter, 'brightness', -1.0, 1.0, 0.05, (val) => config.default.human.filter.brightness = parseFloat(val));
+  objects.menus.filters.addRange('Contrast', config.default.human.filter, 'contrast', -1.0, 1.0, 0.05, (val) => config.default.human.filter.contrast = parseFloat(val));
+  objects.menus.filters.addRange('Sharpness', config.default.human.filter, 'sharpness', 0, 1.0, 0.05, (val) => config.default.human.filter.sharpness = parseFloat(val));
+  objects.menus.filters.addRange('Blur', config.default.human.filter, 'blur', 0, 20, 1, (val) => config.default.human.filter.blur = parseInt(val));
+  objects.menus.filters.addRange('Saturation', config.default.human.filter, 'saturation', -1.0, 1.0, 0.05, (val) => config.default.human.filter.saturation = parseFloat(val));
+  objects.menus.filters.addRange('Hue', config.default.human.filter, 'hue', 0, 360, 5, (val) => config.default.human.filter.hue = parseInt(val));
+  objects.menus.filters.addRange('Pixelate', config.default.human.filter, 'pixelate', 0, 32, 1, (val) => config.default.human.filter.pixelate = parseInt(val));
+  objects.menus.filters.addBool('negative', config.default.human.filter, 'negative');
+  objects.menus.filters.addBool('sepia', config.default.human.filter, 'sepia');
+  objects.menus.filters.addBool('vintage', config.default.human.filter, 'vintage');
+  objects.menus.filters.addBool('kodachrome', config.default.human.filter, 'kodachrome');
+  objects.menus.filters.addBool('technicolor', config.default.human.filter, 'technicolor');
+  objects.menus.filters.addBool('polaroid', config.default.human.filter, 'polaroid');
   objects.menus.filters.toggle();
 
   objects.menus.perf = new Menu(document.body, '');
@@ -211,7 +211,7 @@ async function menuSetup() {
     switch (evt.target.id) {
       case 'video-start': cameraRestart(); break;
       case 'menu-startstop': cameraRestart(); break;
-      case 'menu-facing': config.facing = !config.facing; cameraRestart(); break;
+      case 'menu-facing': config.default.facing = !config.default.facing; cameraRestart(); break;
       case 'menu-models': objects.menus.model.toggle(evt); break;
       case 'menu-parameters': objects.menus.params.toggle(evt); break;
       case 'menu-filters': objects.menus.filters.toggle(evt); break;
@@ -235,11 +235,11 @@ async function xhrFetch(url) {
 async function main() {
   log.debug(window.location.href);
   await user.get();
-  await shared.theme();
-  await shared.done();
+  await config.setTheme();
+  await config.done();
   await menuSetup();
   await cameraSetup();
-  await run.init(config);
+  await run.init(config.default);
 }
 
 window.onload = main;
