55571fd8d0291adb70e2dfbe7e1f7a1abbed1d6e
Vladimir Mandic <mandic00@live.com>
2021-05-30 17:22:12 -0400

    configurable download chunk sizes

--- client/shared/config.ts
+++ client/shared/config.ts
@@ -16,6 +16,8 @@ const config = {
   memory: true,            // set webgl memory hard limit
   autoreload: true,        // auto reload processing window on error
   floatPrecision: true,    // use 32-bit float precision
+  downloadChunkSize: 500,  // number of records downloaded in each chunk
+  namesThumbSize: 96,      // size of person thumbnail
   cacheAssets: true,       // pwa setting
   cacheMedia: false,       // pwa setting
   cacheModels: false,      // pwa setting
